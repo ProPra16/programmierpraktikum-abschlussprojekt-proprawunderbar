ff93a6b6ed043b9640ee1f15b08f073e8d202632
michellewalloschek <Leoynaaa@gmail.com>
2016-07-03 19:14:52 +0200

    Add files via upload
    GUI verschönert, Fenster für compile() eingefügt. (Michelle)

--- src/main/java/tddt/UI/Test_UI.java
+++ src/main/java/tddt/UI/Test_UI.java
@@ -1,17 +1,23 @@
-package tddt.UI;
+package sample.UI;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import tddt.code.Compile;
+//import tddt.code.Compile;
 
 public class Test_UI {
     public static Stage stage = new Stage();
@@ -19,54 +25,108 @@ public class Test_UI {
     public static int height = 1000;
     static TextArea testCode = new TextArea();
 	static TextArea sourceCode = new TextArea();
+	static TextArea compileOutput = new TextArea();
 	public static String status = "writeTest";
-	public static Text a = new Text("Bitte geben sie einen fehlschlagenden Test ein!");
+	public static Text a = new Text("Erstellen sie ihren ersten Test:");
 
     
     public static void runTestUI() {
     	BorderPane borderPane = new BorderPane();
 		GridPane text = new GridPane();
 
+		DropShadow shadow = new DropShadow();
+		shadow.setOffsetY(3.0f);
+		shadow.setColor(Color.DARKGRAY);
+
+		InnerShadow innerShadow = new InnerShadow();
+		innerShadow.setOffsetX(0.5f);
+		innerShadow.setOffsetY(0.5f);
+		innerShadow.setColor(Color.BLACK);
+
+		Blend blend= new Blend();
+		blend.setMode(BlendMode.MULTIPLY);
+		blend.setBottomInput(shadow);
+		blend.setTopInput(innerShadow);
+
 		GridPane buttons = new GridPane();
 		Button save = new Button("Speichern");
 		save.setPrefSize(500,30);
 		save.setFont(Font.font("Verdana",20));
+		save.setEffect(shadow);
+		save.setCache(true);
+		save.setStyle("-fx-font-weight: bold;");
 		Button compile = new Button("Kompilieren");
 		   compile.setOnAction(e -> {
-	        	Compile.compile(sourceCode.getText(),"HalloWelt", testCode.getText(), "HalloWeltTest",status);
+	 //       	Compile.compile(sourceCode.getText(),"HalloWelt", testCode.getText(), "HalloWeltTest",status);
 	        });
 		compile.setPrefSize(500,30);
 		compile.setFont(Font.font("Verdana",20));
+		compile.setEffect(shadow);
+		compile.setCache(true);
+		compile.setStyle("-fx-font-weight: bold;");
 		Button test = new Button("Projekt Testen");
 		test.setPrefSize(500,30);
 		test.setFont(Font.font("Verdana",20));
+		test.setEffect(shadow);
+		test.setCache(true);
+		test.setStyle("-fx-font-weight: bold;");
 
-		a.setFont(Font.font("Verdana", 25));
+		a.setFont(Font.font("Verdana", 30));
+		a.setStyle("-fx-font-weight: bold;");
+		a.setFill(Color.DARKGRAY);
+		a.setEffect(blend);
+		a.setCache(true);
 		text.setAlignment(Pos.CENTER);
 		text.setPadding(new Insets(25,25,25,25));
 		text.add( a , 1,1,1,1);
 		
 		GridPane center = new GridPane();
+		center.setPadding(new Insets(0,25,0,15));
+
 		Text tests = new Text("Tests:");
-		a.setFont(Font.font("Verdana", 15));
+		tests.setFont(Font.font("Verdana", 20));
+		tests.setStyle("-fx-font-weight: bold;");
+		tests.setFill(Color.DARKGRAY);
+		tests.setEffect(blend);
+		tests.setCache(true);
+
 		Text source = new Text("Code:");
-		a.setFont(Font.font("Verdana", 15));
+		source.setFont(Font.font("Verdana", 20));
+		source.setStyle("-fx-font-weight: bold;");
+		source.setFill(Color.DARKGRAY);
+		source.setEffect(blend);
+		source.setCache(true);
+
 		center.add(tests, 0, 0);
 		center.add(source, 1, 0);
+		center.setHgap(10);
+		center.setVgap(10);
 		
-		sourceCode.setMinWidth(width/2);
-		sourceCode.setMinHeight(height-180);
-		testCode.setMinWidth(width/2);
-		testCode.setMinHeight(height-180);
+		sourceCode.setMinWidth((width/2)-25);
+		sourceCode.setMinHeight(height-380);
+		sourceCode.setEffect(blend);
+		sourceCode.setCache(true);
+
+		testCode.setMinWidth((width/2)-25);
+		testCode.setMinHeight(height-380);
+		testCode.setEffect(blend);
+		testCode.setCache(true);
+
+		compileOutput.setMinWidth(width-60);
+		compileOutput.setMaxHeight(540);
+		compileOutput.setEffect(blend);
+		compileOutput.setCache(true);
 		String s = "";
 		testCode.setText(s);
 //		sourceCode.setEditable(false);
 		center.add(testCode, 0, 1);
 		center.add(sourceCode, 1, 1);
+		center.add(compileOutput, 0, 2, 2, 1);
 		
 		borderPane.setCenter(center);
 		borderPane.setTop(text);
 		borderPane.setBottom(buttons);
+		borderPane.setStyle("-fx-background-color: #E0E0E0");
 
 
 		buttons.setAlignment(Pos.TOP_CENTER);
