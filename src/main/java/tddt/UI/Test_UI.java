fa3eab46ead1ce49398fd75a3ddc9a3d5c5de1ad
FelixSchaefer <felix.wolfgang.karl.schaefer@uni-duesseldorf.de>
2016-07-13 20:05:18 +0200

    Timer (hoffentlich) implementiert

--- src/main/java/tddt/UI/Test_UI.java
+++ src/main/java/tddt/UI/Test_UI.java
@@ -23,6 +23,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import tddt.code.Compile;
+import tddt.code.Timer;
 
 public class Test_UI {
 	private static Stage stage = new Stage();
@@ -33,16 +34,19 @@ public class Test_UI {
 	public static TextArea compileOutput = new TextArea();
 	public static String status = "writeTest";
 	public static Text a = new Text("");
-	public static String backup;
-
+	public static String sourceBackup;
+	public static String testBackup;
+	public static Text timerText;
+	public static Timer timer = new Timer(timerText, true, 15);
 
 	public static void runTestUI(String x, Exercise exercise) throws IOException {
 		BorderPane borderPane = new BorderPane();
 		GridPane text = new GridPane();
-		backup = exercise.classCode;
+		sourceBackup = exercise.classCode;
+		testBackup = exercise.testCode;
 
-		String input= new String(String.valueOf(Files.readAllLines(Paths.get(x+"Aufgabenstellung.txt"))));
-		String input2 = input.substring(1, input.length()-1);
+		String input = new String(String.valueOf(Files.readAllLines(Paths.get(x + "Aufgabenstellung.txt"))));
+		String input2 = input.substring(1, input.length() - 1);
 		a.setText(input2);
 
 		DropShadow shadow = new DropShadow();
@@ -54,58 +58,57 @@ public class Test_UI {
 		innerShadow.setOffsetY(0.5f);
 		innerShadow.setColor(Color.BLACK);
 
-		Blend blend= new Blend();
+		Blend blend = new Blend();
 		blend.setMode(BlendMode.MULTIPLY);
 		blend.setBottomInput(shadow);
 		blend.setTopInput(innerShadow);
 
 		GridPane buttons = new GridPane();
 		Button save = new Button("Speichern");
-		save.setPrefSize(500,30);
-		save.setFont(Font.font("Verdana",20));
+		save.setPrefSize(500, 30);
+		save.setFont(Font.font("Verdana", 20));
 		save.setEffect(shadow);
 		save.setCache(true);
 		save.setStyle("-fx-font-weight: bold;");
 
 		Button compile = new Button("Kompilieren");
 		compile.setOnAction(e -> {
-//					System.out.println("Hallo Welt!");
-					compileOutput.setText("");
-			      	Compile.runTests(sourceCode.getText(),exercise.className, testCode.getText(), exercise.testName);
-			      	
+			// System.out.println("Hallo Welt!");
+			compileOutput.setText("");
+			Compile.runTests(sourceCode.getText(), exercise.className, testCode.getText(), exercise.testName);
+
 		});
 		System.out.println(exercise.className);
 		System.out.println(exercise.testName);
-		compile.setPrefSize(500,30);
-		compile.setFont(Font.font("Verdana",20));
+		compile.setPrefSize(500, 30);
+		compile.setFont(Font.font("Verdana", 20));
 		compile.setEffect(shadow);
 		compile.setCache(true);
 		compile.setStyle("-fx-font-weight: bold; -fx-base: #FFFFFF");
 
 		Button returnButton = new Button("Zurück");
-		returnButton.setPrefSize(500,30);
-		returnButton.setFont(Font.font("Verdana",20));
+		returnButton.setPrefSize(500, 30);
+		returnButton.setFont(Font.font("Verdana", 20));
 		returnButton.setEffect(shadow);
 		returnButton.setCache(true);
 		returnButton.setStyle("-fx-font-weight: bold;");
 		returnButton.setOnAction(e -> {
-			if(status.equals("fixTest"))
+			if (status.equals("fixTest"))
 				returnToTest();
-				
-			
+
 		});
-		
+
 		a.setFont(Font.font("Verdana", 30));
 		a.setStyle("-fx-font-weight: bold; -fx-base: #FFFFFF");
 		a.setFill(Color.DARKGRAY);
 		a.setEffect(blend);
 		a.setCache(true);
 		text.setAlignment(Pos.CENTER);
-		text.setPadding(new Insets(25,25,25,25));
-		text.add( a , 1,1,1,1);
+		text.setPadding(new Insets(25, 25, 25, 25));
+		text.add(a, 1, 1, 1, 1);
 
 		GridPane center = new GridPane();
-		center.setPadding(new Insets(0,25,0,15));
+		center.setPadding(new Insets(0, 25, 0, 15));
 
 		Text tests = new Text("Tests:");
 		tests.setFont(Font.font("Verdana", 20));
@@ -115,48 +118,47 @@ public class Test_UI {
 		tests.setCache(true);
 
 		Text source = new Text("Code:");
-		source.setFont(Font.font("Verdana",20));
+		source.setFont(Font.font("Verdana", 20));
 		source.setStyle("-fx-font-weight: bold; -fx-base: #FFFFFF");
 		source.setFill(Color.DARKGRAY);
 		source.setEffect(blend);
 		source.setCache(true);
 
-		Button color1=new Button();
-		color1.setPrefSize(40,40);
-		if(!color1.equals("-fx-base: #FFFFFF")){
-			//hier die abbruchbedingung
+		Button color1 = new Button();
+		color1.setPrefSize(40, 40);
+		if (!color1.equals("-fx-base: #FFFFFF")) {
+			// hier die Abbruchbedingung
 		}
-		Button color2=new Button();
-		if(!color2.equals("-fx-base: #FFFFFF")){
-			//hier die abbruchbedingung
+		Button color2 = new Button();
+		if (!color2.equals("-fx-base: #FFFFFF")) {
+			// hier die Abbruchbedingung
 		}
-		color2.setPrefSize(40,40);
+		color2.setPrefSize(40, 40);
 
 		center.add(tests, 0, 0);
 		center.add(source, 1, 0);
 		center.setHgap(10);
 		center.setVgap(10);
 
-		sourceCode.setMinWidth((width/2)-25);
-		sourceCode.setMinHeight(height-380);
+		sourceCode.setMinWidth((width / 2) - 25);
+		sourceCode.setMinHeight(height - 380);
 		sourceCode.setEffect(blend);
 		sourceCode.setCache(true);
 		sourceCode.setText(exercise.classCode);
 
-		testCode.setMinWidth((width/2)-25);
-		testCode.setMinHeight(height-380);
+		testCode.setMinWidth((width / 2) - 25);
+		testCode.setMinHeight(height - 380);
 		testCode.setEffect(blend);
 		testCode.setCache(true);
 		testCode.setText(exercise.testCode);
-	
-		
-		compileOutput.setMinWidth(width-60);
+
+		compileOutput.setMinWidth(width - 60);
 		compileOutput.setMaxHeight(540);
 		compileOutput.setEffect(blend);
 		compileOutput.setCache(true);
 		compileOutput.setEditable(false);
-	
-//		sourceCode.setEditable(false);
+
+		// sourceCode.setEditable(false);
 		center.add(testCode, 0, 1);
 		center.add(sourceCode, 1, 1);
 		center.add(compileOutput, 0, 2, 2, 1);
@@ -166,42 +168,63 @@ public class Test_UI {
 		borderPane.setBottom(buttons);
 		borderPane.setStyle("-fx-background-color: #E0E0E0");
 
-
 		buttons.setAlignment(Pos.TOP_CENTER);
 		buttons.setPadding(new Insets(20));
-		buttons.add(color1,0,1);
-		buttons.add(save,1,1);
-		buttons.add(compile,2,1);
-		buttons.add(returnButton,3,1);
-		buttons.add(color2,4,1);
+		buttons.add(color1, 0, 1);
+		buttons.add(save, 1, 1);
+		buttons.add(compile, 2, 1);
+		buttons.add(returnButton, 3, 1);
+		buttons.add(color2, 4, 1);
 
 		stage.setScene(new Scene(borderPane, width, height));
 		stage.initStyle(StageStyle.UTILITY);
-		stage.setTitle("Übung"+x);
+		stage.setTitle("Übung" + x);
 		stage.show();
 	}
-	public static void switchStatus(){
-		if(status.equals("writeTest")){
+
+	public static void switchStatus() {
+		if (status.equals("writeTest")) {
+			sourceBackup = sourceCode.getText();
+			testBackup = testCode.getText();
 			status = "fixTest";
+
 			testCode.setEditable(false);
-			testCode.setStyle("-fx-background-color: #E0E0E0");//Farbe wählen
+			testCode.setStyle("-fx-background-color: #E0E0E0"); // Farbe waehlen
 			sourceCode.setEditable(true);
-			sourceCode.setStyle("-fx-background-color: #E0E0E0");//Farbe wählen
+			sourceCode.setStyle("-fx-background-color: #E0E0E0"); // Farbe
+																	// waehlen
 			a.setText("Bitte überarbeiten Sie ihr Programm, sodass alle Tests laufen!");
 		}
-		else if(status.equals("fixTest")){
+
+		else if (status.equals("fixTest")) {
+			sourceBackup = sourceCode.getText();
+			testBackup = testCode.getText();
 			status = "writeTest";
-			backup = sourceCode.getText();
+
 			testCode.setEditable(true);
-			testCode.setStyle("-fx-background-color: #E0E0E0");//Farbe wählen
+			testCode.setStyle("-fx-background-color: #E0E0E0"); // Farbe waehlen
 			sourceCode.setEditable(false);
-			sourceCode.setStyle("-fx-background-color: #E0E0E0");//Farbe wählen
+			sourceCode.setStyle("-fx-background-color: #E0E0E0"); // Farbe
+																	// waehlen
 			a.setText("Bitte geben sie einen fehlschlagenden Test ein!");
 		}
-    }
-	public static void returnToTest(){
-		sourceCode.setText(backup);
+	}
+
+	public static void returnToTest() {
+		sourceCode.setText(sourceBackup);
 		switchStatus();
+	}
+
+	public static void reset() {
+		if (status.equals("writeTest")) {
+			sourceCode.setText(sourceBackup);
+			timer.reset();
+		}
+
+		else if (status.equals("fixTest")) {
+			testCode.setText(testBackup);
+			timer.reset();
+		}
 	}
 
 }
