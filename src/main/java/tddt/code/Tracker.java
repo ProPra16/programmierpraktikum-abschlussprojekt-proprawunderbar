8f62e83e514e0e1f1d26f44950d5db2593eeed1c
FelixSchaefer <felix.wolfgang.karl.schaefer@uni-duesseldorf.de>
2016-07-14 19:16:25 +0200

    Tracker 2.0

--- src/main/java/tddt/code/Tracker.java
+++ src/main/java/tddt/code/Tracker.java
@@ -7,21 +7,24 @@ public class Tracker {
 	
 	public static String status = "test";
 	
-	public class Times {
+	/*public class Times {
 		public int code = 0;
 		public int test = 0;
 		public Times(){}
-	}
+	}*/
 	
-	public static List<Times> times = new ArrayList<Times>();
-	public static List<Integer> testTimes = new ArrayList<>();
-	public static List<Integer> codeTimes = new ArrayList<>();
-	public static int runs = 0;
+	public static List code = new ArrayList();
+	public static List test = new ArrayList();
+	//public static List<Times> times = new ArrayList<Times>();
+	
+	//public Times temp = new Times();
+	static int codeTemp;
+	static int testTemp;
+	
+	static boolean clocking = false;
+	static boolean running = true;
 	
 	public class Timer {
-		
-		boolean clocking = false;
-		boolean running = true;
 		
 		Thread clock = new Thread(() -> {
 			while (running) {
@@ -32,10 +35,10 @@ public class Tracker {
 						
 					}
 					if (status == "code") {
-				//		temp.code++;
+						codeTemp++;
 					}
 					else if (status == "test") {
-					//	temp.test++;
+						testTemp++;
 					}
 				}
 				try {
@@ -49,24 +52,32 @@ public class Tracker {
 	
 	
 	public static void displayTimes() {
-		if (times.size() == 0) return;
-		for (int i = 0; i < times.size(); i++) {
-			
-			System.out.println("Das Schreiben des " + i+1 + ". Tests hat " + times.get(i).test + " Sekunden gedauert.");
-			System.out.println("Das Schreiben des " + i+1 + ". Codes hat " + times.get(i).code + " Sekunden gedauert.");
+		if (code.size() == 0 || test.size() == 0) return;
+		for (int i = 0; i < code.size(); i++) {
+			//Times printTemp = new Times();
+			//printTemp = times.get(i);
+			System.out.println("Das Schreiben des " + i+1 + ". Tests hat " + test.get(i) + " Sekunden gedauert.");
+			System.out.println("Das Schreiben des " + i+1 + ". Codes hat " + code.get(i) + " Sekunden gedauert.");
 		}
 	}
 	
 	public static void switchStatus() {
 		if (status == "code"){
-		//	times.add(temp);
-			//temp.code = 0;
-		//	temp.test = 0;
+			//times.add(temp);
+			code.add(codeTemp);
+			test.add(testTemp);
+			codeTemp = 0;
+			testTemp = 0;
 			status = "test";
 		}
 		
 		else if (status == "test"){
 			status = "code";
 		}
+	}
+	
+	public static void kill() {
+		running = false;
+		clocking = false;
 	}
 }
